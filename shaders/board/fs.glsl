ade9b245a6db3fae824968013c135f5181c5b90e
Ian Bryant <ijb2020syr@gmail.com>
2024-01-03 13:09:54 -0500

    smooth checkerboard

--- shaders/board/fs.glsl
+++ shaders/board/fs.glsl
@@ -11,23 +11,24 @@ uniform float uReflectivity;
 
 out vec4 FragColor;
 
+vec3 tri(vec3 x){
+    return 1.0-abs(2.0*fract(x/2.0)-1.0);
+}
+float checker(vec3 p){ //from Inigo Quilez: https://www.shadertoy.com/view/XlXBWs For some reason I think this version of his looks better than his more updated one. Sharper.
+  vec3 w = max(abs(dFdx(p)), abs(dFdy(p))) + 0.0001; // filter kernel
+  vec3 i = (tri(p+0.5*w)-tri(p-0.5*w))/w;    // analytical integral (box filter)
+  return 0.5 - 0.5*i.x*i.y*i.z;              // xor pattern
+}
+float clampedChecker(vec3 p){
+    return checker(vec3(clamp(p.x,0.01,7.99),0.5,clamp(p.z,0.01,7.99)));
+}
+vec4 getBoardColor(vec3 p){
+    return vec4(vec3(clampedChecker(p)),1.0);
+}
+
 void main() {
-    float cx = clamp(vPosition.x,0.01,7.99); //clamp to fix edge artifacts
-    float cz = clamp(vPosition.z,0.01,7.99);
-    vec4 color;
-    if (mod(floor(cz),2.0)==0.0){
-        if (mod(floor(cx),2.0)==0.0){
-            color = vec4(0.0,0.0,0.0,1.0);
-        } else {
-            color = vec4(1.0,1.0,1.0,1.0);
-        }
-    } else {
-        if (mod(floor(cx),2.0)==0.0){
-            color = vec4(1.0,1.0,1.0,1.0);
-        } else {
-            color = vec4(0.0,0.0,0.0,1.0);
-        }
-    }
+    vec4 color = getBoardColor(vPosition);
+
     vec3 lightDir = normalize(vec3(1.0,-2.0,1.0));
     float light = (max(dot(vNormal,-lightDir),0.0) + uAmbient);
 
--- shaders/reflected_piece/fs.glsl
+++ shaders/reflected_piece/fs.glsl
@@ -13,6 +13,22 @@ uniform float uReflectivity;
 
 out vec4 FragColor;
 
+vec3 tri(vec3 x){
+    return 1.0-abs(2.0*fract(x/2.0)-1.0);
+}
+float checker(vec3 p){ //from Inigo Quilez: https://www.shadertoy.com/view/XlXBWs For some reason I think this version of his looks better than his more updated one. Sharper.
+  vec3 w = max(abs(dFdx(p)), abs(dFdy(p))) + 0.0001; // filter kernel
+  vec3 i = (tri(p+0.5*w)-tri(p-0.5*w))/w;    // analytical integral (box filter)
+  return 0.5 - 0.5*i.x*i.y*i.z;              // xor pattern
+}
+
+float clampedChecker(vec3 p){
+    return checker(vec3(clamp(p.x,0.01,7.99),0.5,clamp(p.z,0.01,7.99)));
+}
+vec4 getBoardColor(vec3 p){
+    return vec4(vec3(clampedChecker(p)),1.0);
+}
+
 void main(){
 
     vec3 lightDir = normalize(vec3(1.0,-2.0,1.0));
@@ -31,22 +47,7 @@ void main(){
     ){
         discard;
     }
-    float cx = clamp(boardPoint.x,0.01,7.99); //clamp to fix edge artifacts
-    float cz = clamp(boardPoint.z,0.01,7.99);
-    vec4 boardColor;
-    if (mod(floor(cz),2.0)==0.0){
-        if (mod(floor(cx),2.0)==0.0){
-            boardColor = vec4(0.0,0.0,0.0,1.0);
-        } else {
-            boardColor = vec4(1.0,1.0,1.0,1.0);
-        }
-    } else {
-        if (mod(floor(cx),2.0)==0.0){
-            boardColor = vec4(1.0,1.0,1.0,1.0);
-        } else {
-            boardColor = vec4(0.0,0.0,0.0,1.0);
-        }
-    }
+    vec4 boardColor = getBoardColor(boardPoint);
 
     FragColor = mix(
         boardColor*boardLight,
