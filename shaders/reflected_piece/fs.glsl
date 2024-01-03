e30188faf6a61d5b49caaecca4bd6e01e35444e0
Ian Bryant <ijb2020syr@gmail.com>
2024-01-02 19:27:31 -0500

    reflections

---
+++ shaders/reflected_piece/fs.glsl
@@ -0,0 +1,54 @@
+#version 300 es
+precision highp float;
+
+in vec3 vPosition;
+in vec3 vInvPos;
+in vec3 vNormal;
+
+uniform vec3 uCamPos;
+uniform samplerCube uSkybox;
+uniform vec4 uColor;
+
+out vec4 FragColor;
+
+void main(){
+
+    vec3 lightDir = normalize(vec3(1.0,-2.0,1.0));
+    float boardLight = (max(dot(vec3(0.0,1.0,0.0),-lightDir),0.0) + 0.75);
+    float pieceLight = (max(dot(vNormal,-lightDir),0.0) + 0.75);
+
+    vec3 ray = normalize(vPosition-uCamPos);
+    vec3 invRay = normalize(vInvPos-uCamPos);
+
+    vec3 boardPoint = uCamPos + ((1.0 - uCamPos.y) / invRay.y) * invRay;
+    if (
+        boardPoint.x < 0.07 || 
+        boardPoint.x > (8.0-0.07) ||
+        boardPoint.z < 0.07 || 
+        boardPoint.z > (8.0-0.07)
+    ){
+        discard;
+    }
+    float cx = clamp(boardPoint.x,0.01,7.99); //clamp to fix edge artifacts
+    float cz = clamp(boardPoint.z,0.01,7.99);
+    vec4 boardColor;
+    if (mod(floor(cz),2.0)==0.0){
+        if (mod(floor(cx),2.0)==0.0){
+            boardColor = vec4(0.0,0.0,0.0,1.0);
+        } else {
+            boardColor = vec4(1.0,1.0,1.0,1.0);
+        }
+    } else {
+        if (mod(floor(cx),2.0)==0.0){
+            boardColor = vec4(1.0,1.0,1.0,1.0);
+        } else {
+            boardColor = vec4(0.0,0.0,0.0,1.0);
+        }
+    }
+
+    FragColor = mix(
+        boardColor*boardLight,
+        mix(uColor*pieceLight,texture(uSkybox,reflect(ray,normalize(vNormal))),0.5),
+        0.5
+    );
+}
--- shaders/reflected_piece/vs.glsl
+++ shaders/reflected_piece/vs.glsl
@@ -7,10 +7,14 @@ uniform vec3 uTranslation;
 uniform mat4 uVP;
 
 out vec3 vPosition;
+out vec3 vInvPos;
 out vec3 vNormal;
 
 void main() {
+    vInvPos = aPosition;
+    vInvPos.y = -vInvPos.y;
+    vInvPos += uTranslation;
     vPosition = aPosition + uTranslation;
-    gl_Position = uVP * vec4(vPosition,1.0);
+    gl_Position = uVP * vec4(vInvPos,1.0);
     vNormal = aNormal;
 }
